c66837b71efea8c86804b1aa987b548c50e5e9df
sluongo718 <sluongo718@yahoo.com>
2021-10-13 03:32:36 -0400

    waitlist controller done

--- app/controllers/wait_lists_controller.rb
+++ app/controllers/wait_lists_controller.rb
@@ -1,2 +1,33 @@
 class WaitListsController < ApplicationController
+
+    def index 
+        waitlists = WaitList.all 
+        render json: waitlists
+    end
+
+    def show 
+        waitlist = WaitList.find(params[:id])
+        
+        render json: waitlist 
+    end
+
+    def create 
+        waitlist = WaitList.new(waitList_params)
+        if waitlist.save 
+            render json: waitlist
+        end
+    end
+
+    def destroy 
+        waitlist = WaitList.find_by(id: params[:id])
+        waitlist.delete
+        render json: {waitlistId: waitlist.id}
+    end
+
+    private 
+
+        def waitlist_params
+            params.require(:wait_lists).permit(:name)
+        end
+
 end
